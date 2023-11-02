34cc99d30983ffe827feb97565d3a59ceb182f6d
Elizeu RS <13ders@gmail.com>
2023-11-02 14:37:39 -0300

    14 - Photo Messages

--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FirebaseDatabase
+import MessageKit
 
 final class DatabaseManager {
   
@@ -419,6 +420,27 @@ extension DatabaseManager {
                 return nil
               }
         
+        var kind: MessageKind?
+        if type == "photo" {
+          // photo
+          guard let imageUrl = URL(string: content),
+          let placeHolder = UIImage(systemName: "plus") else {
+            return nil
+          }
+          let media = Media(url: imageUrl,
+                            image: nil,
+                            placeholderImage: placeHolder,
+                            size: CGSize(width: 300, height: 300))
+          kind = .photo(media)
+        }
+        else {
+          kind = .text(content)
+        }
+        
+        guard let finalKind = kind else {
+          return nil
+        }
+        
         let sender = Sender(photoURL: "",
                             senderId: senderEmail,
                             displayName: name)
@@ -426,7 +448,7 @@ extension DatabaseManager {
         return Message(sender: sender,
                        messageId: messageID,
                        sentDate: date,
-                       kind: .text(content))
+                       kind: finalKind)
       })
       
       
@@ -466,7 +488,10 @@ return
         message = messageText
       case .attributedText(_):
         break
-      case .photo(_):
+      case .photo(let mediaItem):
+        if let targetUrlString = mediaItem.url?.absoluteString {
+          message = targetUrlString
+        }
         break
       case .video(_):
         break
